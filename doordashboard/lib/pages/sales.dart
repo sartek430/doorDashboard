ab9275e10b6170bec2d8890468926184e5f8b8ea
MireilleDeMarseille <testprivacy@users.noreply.github.com>
2024-02-15 11:40:39 +0100

    THE ENNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNND

--- doordashboard/lib/pages/sales.dart
+++ doordashboard/lib/pages/sales.dart
@@ -1,7 +1,10 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class Order {
+class Order {  
+  String id;
   String status;
   String product;
   double nombreVendu;
@@ -9,7 +12,9 @@ class Order {
   double totalPrice;
 
   Order(
-      {required this.status,
+      {     
+      required this.id,
+      required this.status,
       required this.product,
       required this.price,
       required this.totalPrice,
@@ -20,12 +25,12 @@ class SalesPage extends StatefulWidget {
   const SalesPage({super.key});
 
   @override
-  // ignore: library_private_types_in_public_api
+ 
   _SalesPageState createState() => _SalesPageState();
 }
 
 class _SalesPageState extends State<SalesPage> {
-  List<Order> orders = [];
+  List<Order> tempOrders = [];
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
 
   @override
@@ -40,18 +45,29 @@ class _SalesPageState extends State<SalesPage> {
           if (!snapshot.hasData) {
             return const Center(child: CircularProgressIndicator());
           }
+          tempOrders = []; // Réinitialiser la liste temporaire à chaque fois que les données changent
           final orders = snapshot.data!.docs;
+          for (final order in orders) {
+            final orderData = order.data() as Map<String, dynamic>;
+            final newOrder = Order(
+              id: order.id,
+              status: orderData['status'],
+              product: orderData['product'],
+              price: orderData['price'],
+              totalPrice: orderData['totalPrice'],
+              nombreVendu: orderData['nombreVendu'],
+            );
+            tempOrders.add(newOrder);
+          }
+          
           return ListView.builder(
-            itemCount: orders.length,
+            itemCount: tempOrders.length,
             itemBuilder: (context, index) {
-              final orderData = orders[index].data() as Map<String, dynamic>;
-              final product = orderData['product'];
-              final status = orderData['status'];
-              final price = orderData['price'];
+              final order = tempOrders[index];
               return ListTile(
-                title: Text(product),
-                subtitle: Text('Status: $status'),
-                trailing: Text('Price: \$${price.toStringAsFixed(2)}'),
+                title: Text(order.product),
+                subtitle: Text('Status: ${order.status}'),
+                trailing: Text('Price: \$${order.price.toStringAsFixed(2)}'),
                 onTap: () {
                   _editOrder(index);
                 },
@@ -72,203 +88,223 @@ class _SalesPageState extends State<SalesPage> {
     );
   }
 
-  void _addOrder() {
-    showDialog(
-      context: context,
-      builder: (context) {
-        String status = '';
-        String product = '';
-        double nombreVendu = 0.0;
-        double price = 0.0;
-        double totalPrice = 0.0;
+void _addOrder() {
+  showDialog(
+    context: context,
+    builder: (context) {        
+      String status = '';
+      String product = '';
+      double nombreVendu = 0.0;
+      double price = 0.0;
+      double totalPrice = 0.0;
 
-        return AlertDialog(
-          title: const Text('Ajouter une vente'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                decoration: const InputDecoration(labelText: 'Status'),
-                onChanged: (value) {
-                  status = value;
-                },
-              ),
-              TextField(
-                decoration: const InputDecoration(labelText: 'Produit'),
-                onChanged: (value) {
-                  product = value;
-                },
-              ),
-              TextField(
-                decoration: const InputDecoration(labelText: 'Nombre Vendu'),
-                keyboardType: TextInputType.number,
-                onChanged: (value) {
-                  nombreVendu = double.parse(value);
-                },
-              ),
-              TextField(
-                decoration: const InputDecoration(labelText: 'Prix'),
-                keyboardType: TextInputType.number,
-                onChanged: (value) {
-                  price = double.parse(value);
-                },
-              ),
-              TextField(
-                decoration: const InputDecoration(labelText: 'Prix Total'),
-                keyboardType: TextInputType.number,
-                onChanged: (value) {
-                  totalPrice = double.parse(value);
-                },
-              ),
-            ],
+      return AlertDialog(
+        title: const Text('Ajouter une vente'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              decoration: const InputDecoration(labelText: 'Status'),
+              onChanged: (value) {
+                status = value;
+              },
+            ),
+            TextField(
+              decoration: const InputDecoration(labelText: 'Produit'),
+              onChanged: (value) {
+                product = value;
+              },
+            ),
+            TextField(
+              decoration: const InputDecoration(labelText: 'Nombre Vendu'),
+              keyboardType: TextInputType.number,
+              onChanged: (value) {
+                nombreVendu = double.parse(value);
+              },
+            ),
+            TextField(
+              decoration: const InputDecoration(labelText: 'Prix'),
+              keyboardType: TextInputType.number,
+              onChanged: (value) {
+                price = double.parse(value);
+              },
+            ),
+            TextField(
+              decoration: const InputDecoration(labelText: 'Prix Total'),
+              keyboardType: TextInputType.number,
+              onChanged: (value) {
+                totalPrice = double.parse(value);
+              },
+            ),
+          ],
+        ),
+        actions: [
+          TextButton(
+            child: const Text('Annuler'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
           ),
-          actions: [
-            TextButton(
-              child: const Text('Annuler'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            TextButton(
-              child: const Text('Ajouter'),
-              onPressed: () async {
-                setState(() {
-                  orders.add(
-                    Order(
-                      status: status,
-                      product: product,
-                      nombreVendu: nombreVendu,
-                      price: price,
-                      totalPrice: totalPrice,
-                    ),
-                  );
-                });
+          TextButton(
+            child: const Text('Ajouter'),
+            onPressed: () async {
+              final newOrder = Order(
+                id: '',
+                status: status,
+                product: product,
+                nombreVendu: nombreVendu,
+                price: price,
+                totalPrice: totalPrice,
+              );
 
-                await FirebaseFirestore.instance.collection('sales').add({
-                  'status': status,
-                  'product': product,
-                  'nombreVendu': nombreVendu,
-                  'price': price,
-                  'totalPrice': totalPrice
-                });
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
+              final docRef = await FirebaseFirestore.instance.collection('sales').add({                  
+                'status': status,
+                'product': product,
+                'nombreVendu': nombreVendu,
+                'price': price,
+                'totalPrice': totalPrice
+              });
 
-  void _editOrder(int index) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        String status = orders[index].status;
-        String product = orders[index].product;
-        double price = orders[index].price;
-        double totalPrice = orders[index].totalPrice;
-        double nombreVendu = orders[index].nombreVendu;
+              newOrder.id = docRef.id;
 
-        return AlertDialog(
-          title: const Text('Modifier une commande'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                decoration: const InputDecoration(labelText: 'Status'),
-                onChanged: (value) {
-                  status = value;
-                },
-                controller: TextEditingController(text: status),
-              ),
-              TextField(
-                decoration: const InputDecoration(labelText: 'Produit'),
-                onChanged: (value) {
-                  product = value;
-                },
-                controller: TextEditingController(text: product),
-              ),
-              TextField(
-                decoration: const InputDecoration(labelText: 'NombreVendu'),
-                keyboardType: TextInputType.number,
-                onChanged: (value) {
-                  nombreVendu = double.parse(value);
-                },
-                controller: TextEditingController(text: nombreVendu.toString()),
-              ),
-              TextField(
-                decoration: const InputDecoration(labelText: 'Prix'),
-                keyboardType: TextInputType.number,
-                onChanged: (value) {
-                  price = double.parse(value);
-                },
-                controller: TextEditingController(text: price.toString()),
-              ),
-              TextField(
-                decoration: const InputDecoration(labelText: 'Prix Total'),
-                keyboardType: TextInputType.number,
-                onChanged: (value) {
-                  totalPrice = double.parse(value);
-                },
-                controller: TextEditingController(text: totalPrice.toString()),
-              ),
-            ],
+              setState(() {
+                tempOrders.add(newOrder); 
+              });
+
+              Navigator.of(context).pop();
+            },
           ),
-          actions: [
-            TextButton(
-              child: const Text('Annuler'),
-              onPressed: () {
-                Navigator.of(context).pop();
+        ],
+      );
+    },
+  );
+}
+
+
+void _editOrder(int index) {
+  showDialog(
+    context: context,
+    builder: (context) {
+      String status = tempOrders[index].status;
+      String product = tempOrders[index].product;
+      double price = tempOrders[index].price;
+      double totalPrice = tempOrders[index].totalPrice;
+      double nombreVendu = tempOrders[index].nombreVendu;
+
+      return AlertDialog(
+        title: const Text('Modifier une commande'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              decoration: const InputDecoration(labelText: 'Status'),
+              onChanged: (value) {
+                status = value;
               },
+              controller: TextEditingController(text: status),
             ),
-            TextButton(
-              child: const Text('Sauvegarder'),
-              onPressed: () {
-                setState(() {
-                  orders[index].status = status;
-                  orders[index].product = product;
-                  orders[index].price = price;
-                  orders[index].totalPrice = totalPrice;
-                  orders[index].nombreVendu = nombreVendu;
-                });
-                Navigator.of(context).pop();
+            TextField(
+              decoration: const InputDecoration(labelText: 'Produit'),
+              onChanged: (value) {
+                product = value;
               },
+              controller: TextEditingController(text: product),
+            ),
+            TextField(
+              decoration: const InputDecoration(labelText: 'NombreVendu'),
+              keyboardType: TextInputType.number,
+              onChanged: (value) {
+                nombreVendu = double.parse(value);
+              },
+              controller: TextEditingController(text: nombreVendu.toString()),
+            ),
+            TextField(
+              decoration: const InputDecoration(labelText: 'Prix'),
+              keyboardType: TextInputType.number,
+              onChanged: (value) {
+                price = double.parse(value);
+              },
+              controller: TextEditingController(text: price.toString()),
+            ),
+            TextField(
+              decoration: const InputDecoration(labelText: 'Prix Total'),
+              keyboardType: TextInputType.number,
+              onChanged: (value) {
+                totalPrice = double.parse(value);
+              },
+              controller: TextEditingController(text: totalPrice.toString()),
             ),
           ],
-        );
-      },
-    );
-  }
+        ),
+        actions: [
+          TextButton(
+            child: const Text('Annuler'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          TextButton(
+            child: const Text('Sauvegarder'),
+            onPressed: () async {
+              setState(() {
+                tempOrders[index].status = status;
+                tempOrders[index].product = product;
+                tempOrders[index].price = price;
+                tempOrders[index].totalPrice = totalPrice;
+                tempOrders[index].nombreVendu = nombreVendu;
+              });
 
-  void _deleteOrder(int index) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text('Supprimer une vente'),
-          content:
-              const Text('Êtes vous sûrs de vouloir supprimer cette vente?'),
-          actions: [
-            TextButton(
-              child: const Text('Annuler'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            TextButton(
-              child: const Text('Supprimer'),
-              onPressed: () {
-                setState(() {
-                  orders.removeAt(index);
-                });
+              // Mettre à jour la commande dans la base de données Firebase
+              final orderRef = firestore.collection('sales').doc(tempOrders[index].id);
+              await orderRef.update({
+                'status': status,
+                'product': product,
+                'price': price,
+                'totalPrice': totalPrice,
+                'nombreVendu': nombreVendu,
+              });
 
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
+
+  
+
+void _deleteOrder(int index) {
+  final order = tempOrders[index];
+  final orderRef = firestore.collection('sales').doc(order.id);
+
+  showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text('Supprimer une vente'),
+        content: const Text('Êtes-vous sûr de vouloir supprimer cette vente ?'),
+        actions: [
+          TextButton(
+            child: const Text('Annuler'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          TextButton(
+            child: const Text('Supprimer'),
+            onPressed: () {
+              orderRef.delete();
+              setState(() {
+                tempOrders.removeAt(index);
+              });
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
 }
